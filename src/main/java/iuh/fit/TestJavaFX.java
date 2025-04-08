c7d1771bb54b1ffb77ef397b6f295c7ed8857c4c
Hoang Son <sonjiny2004@gmail.com>
2025-04-08 20:58:11 +0700

    HS update

--- src/main/java/iuh/fit/TestJavaFX.java
+++ src/main/java/iuh/fit/TestJavaFX.java
@@ -11,9 +11,7 @@ public class TestJavaFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/fxml/ThongKeDoanhThu_gui.fxml"));
-//            Parent root = FXMLLoader.load(getClass().getResource("/fxml/ThongKeSanPham_gui.fxml"));
-//            Parent root = FXMLLoader.load(getClass().getResource("/fxml/BanHang_gui.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/BanHang_gui.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.setMaximized(true);
